0b0713bac74dc213a03bd44f64f13247cb616f3c
Sam <Sam.151@live.com>
2022-01-06 02:10:28 +0100

    Cleaned up main scene - Added Doll

---
+++ vr_project/Assets/Interactive Storytelling VR/Scripts/LockToOrigin.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem.Sample;
+
+public class LockToOrigin : MonoBehaviour
+{
+    GameObject OriginalLocation;
+    //Transform newTransform;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        //Debug.Log(name + ": " + transform.TransformPoint(GetComponent<LockToPoint>().transform.localPosition));
+        OriginalLocation = new GameObject(name + "'s Original Transform");
+        OriginalLocation.transform.position = transform.position;
+        OriginalLocation.transform.eulerAngles = transform.eulerAngles;
+        OriginalLocation.transform.SetParent(transform.parent);
+        GetComponent<LockToPoint>().snapTo = OriginalLocation.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // Debug.Log(name + "'s World Position: " + newTransform.position);
+    }
+}
