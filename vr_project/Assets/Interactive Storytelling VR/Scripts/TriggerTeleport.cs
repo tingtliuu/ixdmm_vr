c58d18eedaa5ae0cbf2c64449ca547179b6ed8e6
Sam <Sam.151@live.com>
2022-01-06 07:31:13 +0100

    Added Fade In/Out

---
+++ vr_project/Assets/Interactive Storytelling VR/Scripts/TriggerTeleport.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class TriggerTeleport : MonoBehaviour
+{
+    bool startFade = false;
+    public int fadeTimer = 2;
+    public Transform Player;
+    public Transform TargetLocation;
+
+    private void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            startFade = !startFade;
+
+            if (startFade)
+            {
+                SteamVR_Fade.Start(Color.clear, 0);
+                SteamVR_Fade.Start(Color.black, fadeTimer);
+                StartCoroutine(teleportPlayer(fadeTimer));
+            }
+            else
+            {
+                SteamVR_Fade.Start(Color.black, 0);
+                SteamVR_Fade.Start(Color.clear, fadeTimer);
+            }
+           
+        }
+    }
+
+    IEnumerator teleportPlayer(float s)
+    {
+        yield return new WaitForSecondsRealtime(s);
+        Player.position = TargetLocation.position;
+        Player.eulerAngles = TargetLocation.eulerAngles;
+    }
+}
