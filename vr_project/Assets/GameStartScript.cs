22d113d679858dafc86511e5eb1e661ce40493cf
Sam <Sam.151@live.com>
2022-01-13 02:59:54 +0100

    Getting Sleepy

---
+++ vr_project/Assets/GameStartScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStartScript : MonoBehaviour
+{
+    [SerializeField] GameObject GameManager;
+    private void OnTriggerEnter(Collider other)
+    {
+        GameManager.GetComponent<GameManager>().gameStart = true;
+        GetComponent<GameStartScript>().enabled = false;
+    }
+}
